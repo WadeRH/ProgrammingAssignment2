6423f2d6535f78762e2e3eaae773951c7cd8881a
Wade Hansen <wade.hansen+github@gmail.com>
2014-12-20 20:52:49 -0700

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment #2
+## Wade Hansen - 2014 Dec 20
 
-## Write a short comment describing this function
+## Write the following functions:
+## 1.makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## 2.cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+      ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+      ## retrieve the inverse from the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+## makeCacheMatrix takes matrix input, it also contains 4 subfunctions
+      ## set - inputs the matrix and stores
+      ## get - returns matrix
+      ## setinv - stores inverse matrix
+      ## getinv - returns inverse matrix
+
+
+makeCacheMatrix <- function (x = matrix()) {
+      i <- NULL
+      set <- function (y) {
+            x <<- y
+            i <<- NULL
+      }
+      get <- function() x
+      setinv <- function(solve) i <<- solve
+      getinv <- function() i
+      list (set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes input of the result of the  makeCacheMatrix function, it looks to see if the inverse cache exists already
+      ## if so it sends a message indicating the use of cached data and returning the inverse matrix
+      ## if not it does the calculates the inverse matrix, stores it and returns inverse matrix
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      i <- x$getinv()
+      if(!is.null(i)) {
+            message("getting cached data")
+            return(i)
+      }
+      data <- x$get()
+      i <- solve(data, ...)
+      x$setinv(i)
+      i
 }
